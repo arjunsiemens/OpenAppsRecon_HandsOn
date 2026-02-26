a9b2728982df7fad56bbd732a125d91c0c23d0c4
Arjun Bhattacharya <arjun.bhattacharya@siemens-healthineers.com>
2026-02-26 11:26:44 +0530

    Update recon_nparray.py

--- OpenRecon/recon_nparray.py
+++ OpenRecon/recon_nparray.py
@@ -7,10 +7,23 @@ class ReconFromNPArray:
     
     def perform_recon(self):
         data = np.abs(np.fft.ifftshift(np.fft.ifft2(self.rawdata_array)))
+        # data = np.abs(np.fft.ifftshift(np.fft.ifft2(self.rawdata_array, axes=(1 ,2)), axes=(1 ,2)))
+        
+        if ((np.ndim(data)>2)) : 
+            # if the number of dimensions are greater than 2 then assumption is that zeroth dimension is channel
+            # Sum of squares coil combination
+            data = np.abs(data)
+            data = np.square(data)
+            data = np.sum(data, axis=0)
+            data = np.sqrt(data)
         
         # Normalize and convert to int16
-        data *= 32767/data.max()
+        mymaxval = 4095
+        data *= mymaxval/data.max()
         data = np.around(data)
+
+        #data = mymaxval/2-data
+        data = np.abs(data)
         data = data.astype(np.int16)
         #plt.imshow(data)
         return data
